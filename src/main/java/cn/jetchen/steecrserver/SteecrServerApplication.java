477654245387cece7198d1d07dcfd429e58faaf2
jet <jet.chen@wailianvisa.com>
2019-07-10 15:23:57 +0800

    first commit

---
+++ src/main/java/cn/jetchen/steecrserver/SteecrServerApplication.java
@@ -0,0 +1,18 @@
+package cn.jetchen.steecrserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+* @Description: 启动类
+* @Author: Jet.Chen
+* @Date: 2019/7/10 15:23
+*/
+@SpringBootApplication
+public class SteecrServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SteecrServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/jetchen/steecrserver/SteecrServerApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.jetchen.steecrserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SteecrServerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
